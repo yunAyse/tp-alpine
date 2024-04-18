778ec58f8a2e9c391a06adcd950eac43e886d700
yunAyse <aysepc0@gmail.com>
2024-04-18 17:10:53 +0200

    structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import './index.css';
+
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+
+const App = () => {
+  return (
+    <>
+      <div className="App">
+        <Header />
+          
+        <Hero/>
+      </div>
+    </>
+  );
+}
+
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+import alpineLogo from '../Assets/images/sources-homepage/logo/logo-white.png';
+
+const Header = () => {
+  return (
+    <header className="bg-slate-900">
+    <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div className="flex lg:flex-1">
+        <a href="#" className="-m-1.5 p-1.5"/>
+          <span className="sr-only">Your Company</span>
+          <img className="h-100 w-50" src={alpineLogo} alt=""/>
+      </div>
+      <div className="flex lg:hidden">
+        <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+          <span className="sr-only">Open main menu</span>
+          <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </button>
+      </div>
+      <div className="hidden lg:flex lg:gap-x-12">
+        {/* </div> */}
+
+
+  
+        <a href="#" className="text-sm font-bold leading-6 text-slate-200">Modèle</a>
+        <a href="#" className="text-sm font-bold leading-6 text-slate-200">Configuration</a>
+        <a href="#" className="text-sm font-bold leading-6 text-slate-200">Galerie</a>
+        <a href="#" className="text-sm font-bold leading-6 text-slate-200">Boutique</a>
+      </div>
+      <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        <a href="#" className="text-sm font-semibold leading-6 text-slate-200">Log in <span aria-hidden="true">&rarr;</span></a>
+      </div>
+    </nav>
+    {/* <!-- Mobile menu, show/hide based on menu open state. --> */}
+    <div className="lg:hidden" role="dialog" aria-modal="true">
+      {/* <!-- Background backdrop, show/hide based on slide-over state. --> */}
+      <div className="fixed inset-0 z-10"></div>
+      <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div className="flex items-center justify-between">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
+          </a>
+          <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
+            <span className="sr-only">Close menu</span>
+            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
+        <div className="mt-6 flow-root">
+          <div className="-my-6 divide-y divide-gray-500/10">
+            <div className="space-y-2 py-6">
+              <div className="-mx-3">
+                
+              </div>
+              <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Modèle</a>
+              <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Configuration</a>
+              <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Galerie</a>
+              <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Boutique</a>
+            </div>
+            <div className="py-6">
+              <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  )
+}
+
+export default Header
